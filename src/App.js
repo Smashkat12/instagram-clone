ee4dce90e742b20ed3e4201d41407baef2b93a27
Smashkat12 <smashkat12@gmail.com>
2020-12-02 17:45:41 +0200

    header + logo + background styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className="app__header">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="instagram logo"
+          className="app__headerImage"
+        />
+      </div>
+
+      {/* Post */}
+      {/* Post */}
     </div>
   );
 }
